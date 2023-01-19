415529de19d3ca361e9478a63937d2b63896bfd1
Antoine de Fourcroy <antoine.defourcroy@gmail.com>
2023-01-19 20:23:01 +0100

    Moves readme to root

--- cypress/support/vault.ts
+++ cypress/support/vault.ts
@@ -1,3 +1,3 @@
-export const ACCOUNT = '<enter account name here>'
-export const USERNAME = '<enter username here>'
-export const PASSWORD = '<enter password here>'
+export const ACCOUNT = 'qauser'
+export const USERNAME = 'manager'
+export const PASSWORD = '0eaab6'
